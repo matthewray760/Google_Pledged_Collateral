dfdc4d6336d9949ade59c9c13b840250d9e9bf73
matthewray760 <165307255+matthewray760@users.noreply.github.com>
2026-01-04 14:43:58 -0700

    Create config.py

---
+++ configurations/config.py
@@ -0,0 +1,15 @@
+BEGIN_BALANCE_BLACKROCK = True #'True' will incorporate being_date into the logic. 'False' will esentially reflect beginning balances as zero
+BEGIN_BALANCE_FIDELITY = True #'True' will incorporate being_date into the logic. 'False' will esentially reflect beginning balances as zero
+USE_SQL_BLACKROCK = True
+USE_SQL_FIDELITY = True
+
+
+
+#If SQL is not working, set use_sql to false and upload file to inputs
+FILENAME_BLACKROCK = ''  
+FILENAME_FIDELITY =''
+
+
+
+
+
